9e402ad51b0ed50a22bf53363f2ab31df37e95c1
MaxwelSantana <maxwelsant96@icloud.com>
2021-09-28 14:10:01 -0300

    added context api to store products in the basket

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './Subtotal.css';
+import NumberFormat from 'react-number-format';
+import { useStateValue } from './StateProvider';
 
 function Subtotal() {
+    const [{ basket }] = useStateValue();
+
+    const items = basket.length;
+    const total = basket.reduce((acc, item) => item.price + acc, 0);
+    console.log(basket);
     return (
         <div className="subtotal">
-            <p>
-                Subtotal (0 items): <strong>0</strong>
-            </p>
-            <small className="subtotal__gift">
-                <input type="checkbox" />
-                This order contains a gift
-            </small>
+            <NumberFormat
+                renderText={(value) => (
+                    <>
+                        <p>
+                            Subtotal ({items} items): <strong>{total}</strong>
+                        </p>
+                        <small className="subtotal__gift">
+                            <input type="checkbox" />
+                            This order contains a gift
+                        </small>
+                    </>
+                )}
+                decimalScale={2}
+                value={0}
+                displayType={'text'}
+                thousandSeparator={true}
+                prefix={'$'}
+            />
+            <button>Proceed to Checkout</button>
         </div>
     );
 }
