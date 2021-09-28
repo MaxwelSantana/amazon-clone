4d43edfd8f76a7d0246b64e13a18c4059379d1e0
MaxwelSantana <maxwelsant96@icloud.com>
2021-09-27 21:42:59 -0300

    working on checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from './Subtotal';
 
 function Checkout() {
     return (
         <div className="checkout">
-            <h1>Checkout page</h1>
+            <div className="checkout__left">
+                <img
+                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                    alt="Adsense banner"
+                    className="checkout__ad"
+                />
+
+                <div>
+                    <h2 className="checkout__title">Your shopping Basket</h2>
+                </div>
+            </div>
+            <div className="checkout__right">
+                <Subtotal />
+                <h2>The subtotal will go here</h2>
+            </div>
         </div>
     );
 }
