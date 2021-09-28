cdc7ec2f7ec4d8a92d322acbafc4fb9f7ea201af
MaxwelSantana <maxwelsant96@icloud.com>
2021-09-27 21:23:43 -0300

    added routes, starting checkout page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Checkout from './Checkout';
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <Home />
-        </div>
+        <Router>
+            <div className="App">
+                <Header />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/checkout">
+                        <Checkout />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Checkout.css';
+
+function Checkout() {
+    return (
+        <div className="checkout">
+            <h1>Checkout page</h1>
+        </div>
+    );
+}
+
+export default Checkout;
