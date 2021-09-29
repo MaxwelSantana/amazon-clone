66c915382757fedd6bd031f4890efb53f4d57e58
MaxwelSantana <maxwelsant96@icloud.com>
2021-09-29 16:27:20 -0300

    orders page

--- src/Orders.js
+++ src/Orders.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import { collection, getDocs, orderBy, query } from '@firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { db } from './firebase';
+import Order from './Order';
 import './Orders.css';
+import { useStateValue } from './StateProvider';
 
 function Orders() {
+    const [{ user }] = useStateValue();
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        const getOrders = async () => {
+            const ordersRef = collection(db, 'users', user?.uid, 'orders');
+            const q = query(ordersRef, orderBy('created', 'desc'));
+            const querySnapshot = await getDocs(q);
+            setOrders(
+                querySnapshot.docs.map((doc) => {
+                    return {
+                        id: doc.id,
+                        data: doc.data(),
+                    };
+                }),
+            );
+        };
+        if (user) getOrders();
+        else setOrders([]);
+    }, [user]);
     return (
         <div className="orders">
             <h1>Your Orders</h1>
+
+            <div className="orders__order">
+                {orders?.map((order) => (
+                    <Order key={order.id} order={order} />
+                ))}
+            </div>
         </div>
     );
 }
