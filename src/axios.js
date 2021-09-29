e4ed135338ca8b5f07e50d8eb7f69eb50026dffb
MaxwelSantana <maxwelsant96@icloud.com>
2021-09-29 16:53:44 -0300

    deploy functions and hosting

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-3842b/us-central1/api',
+    baseURL: 'https://us-central1-clone-3842b.cloudfunctions.net/api',
+    // baseURL: 'http://localhost:5001/clone-3842b/us-central1/api',
 });
 
 export default instance;
+
+// https://us-central1-clone-3842b.cloudfunctions.net/api
